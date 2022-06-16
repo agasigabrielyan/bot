a143cf4d204e30b8dbab1e51ff7531b02de6acca
agasigabrielyan <agasi79@list.ru>
2022-06-16 17:31:08 +0300

    Сделать блок, так чтобы он свою длинну по длине текста.

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 export default function Message(props) {
 
-    const styles = {
-        maxWidth: '100%'
-    };
-
-    if(props.message.length < 50) {
-        styles.maxWidth = '50%';
-    } else if(props.message.length > 50 && props.message.length < 75) {
-        styles.maxWidth = '75%';
-    }
-
     return (
-        <div style={styles} className="box__message">
+        <div className="box__message">
             <div dangerouslySetInnerHTML={{ __html: props.message }}>
 
             </div>
